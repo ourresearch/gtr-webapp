0a47c22587889f6306d1e91d27caecd4c9ebe053
Jason Priem <jp@jasonpriem.org>
2018-03-25 15:31:49 -0700

    tweaks to node server.

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const serveStatic = require('serve-static');
 let app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
+
+  app.get('*', function(req, res) {
+      res.sendfile('./dist/index.html');
+  });
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log('Listening on port ' + port)
