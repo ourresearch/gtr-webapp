58a675f90fe9b0abc3d9943ebdd4cb26a7ee9cdc
Jason Priem <jp@jasonpriem.org>
2018-06-24 12:57:42 -0700

    list of things research can be about.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <div class="tagline">
       Read and understand peer-reviewed research about
-      <vue-typer text='climate change'></vue-typer>
+      <vue-typer :text='["climate change", "self-driving cars", "probiotics", "colonizing Mars", "gene therapy", "low-carb diets", "clean energy", "head trauma", "quantum computers", "caffeine", "CRISPR", "vikings", "medical cannabis", "diabetes" ]'></vue-typer>
     </div>
   </div>
 </template>
