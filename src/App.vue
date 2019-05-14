3b72ba6c257dbca5642d51d5b8f9d96e34b64e53
Jason Priem <jp@jasonpriem.org>
2019-05-13 21:55:11 -0700

    footer works.

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,6 @@
 
     <main-header />
 
-
-
-
     <v-content>
       <v-container fluid fill-height class="pa-0">
         <v-layout
@@ -19,10 +16,9 @@
         </v-layout>
       </v-container>
     </v-content>
-    <v-footer
-    height="auto">
-      <main-footer />
-    </v-footer>
+
+    <main-footer></main-footer>
+
   </v-app>
 
 
@@ -46,7 +42,7 @@
       },
       metaInfo: {
         // if no subcomponents specify a metaInfo.title, this title will be used
-        title: 'Get the research'
+        title: 'Get The Research'
       }
   }
 </script>
@@ -60,15 +56,6 @@
 
 
 
-
-
-
-  $primary-color: #FF3D00;
-
-  $screen-xsmall: 600px;
-  $screen-small: 960px;
-
-
   html {
     &.noscroll {
       overflow: hidden
@@ -80,54 +67,9 @@
     font-size: 16px;
     font-family: Lato, sans-serif;
     &.noscroll {
-      overflow: hidden
-    }
-
-
-    .container {
-      line-height: 1.3;
-      font-size: 16px;
-    }
-
-    hr {
-      opacity: .1;
-    }
-    code {
-      padding: 2px 5px;
-      border: 1px solid #ddd;
-      border-radius: 3px;
-      font-size: 90%;
-    }
-    h1 {
-      margin-top: 50px;
-    }
-    h2 {
-      margin-top: 50px;
-    }
-    .md-button.md-primary {
-     //   background-color: $primary-color !important;
-    }
-  }
-  div.page {
-    font-size: 18px;
-    line-height: 1.5;
-    max-width: 800px;
-    margin: 0 auto;
-    padding-bottom: 100px;
-    min-height: 80vh;
-  }
-
-
-  .vue-typer {
-    .custom.char {
-      /*color: #fff;*/
+      overflow: hidden;
     }
   }
 
-  div.mc-banner {
-    bottom: 0 !important;
-    background: rgba(255,255,255, .95);
-    top: auto;
-  }
 
 </style>
--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,61 +1,35 @@
 <template>
-    <div class="site-footer">
-        <div class="content md-layout md-gutter">
-            <div class="col md-layout-item md-size-30 md-xsmall-size-100 products">
-                <div>
+    <v-footer dark height="auto" class="site-footer d-block pa-4">
+        <div class="headline pb-2">
+            <a class="mr-3" href="https://twitter.com/gettheresearch">
+                <i class="fab fa-twitter"></i>
+            </a>
+            <a class="mx-3" href="mailto:team@impactstory.org">
+                <i class="far fa-envelope"></i>
+            </a>
+            <a class="mx-3" href="https://github.com/impactstory">
+                <i class="fab fa-github"></i>
+            </a>
+            <a class="faq ml-1" href="https://github.com/impactstory">
+                FAQ
+            </a>
 
-                </div>
-                <div>
-                    <ul class="contact-links">
-                        <li>
-                            <a href="https://twitter.com/gettheresearch">
-                                <i class="fab fa-twitter"></i>
-                                <span class="text">
-                                    Twitter
-                                </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="mailto:team@impactstory.org">
-                                <i class="far fa-envelope"></i>
-                                <span class="text">Email</span>
-                            </a>
-
-                        </li>
-                        <li>
-                            <a href="https://github.com/impactstory">
-                                <i class="fab fa-github"></i>
-                                <span class="text">
-                                    GitHub
-                                </span>
-                            </a>
-
-                        </li>
-                    </ul>
-
-                </div>
-            </div>
-
-
-
-            <div class="col md-layout-item md-xsmall-size-100 about">
-            </div>
-
-            <div class="col md-layout-item md-size-50 md-xsmall-size-100 about">
-                <div>
-                    Built with <i class="far fa-heart"></i> at <a href="http://impactstory.org">Impactstory.</a>
-                </div>
-                <hr>
-
-                <div>
-                    Made possible by the support of <a href="https://www.arcadiafund.org.uk/">Arcadia</a> – a charitable fund of Lisbet Rausing and Peter Baldwin.
-
-                </div>
-            </div>
         </div>
 
 
-    </div>
+
+
+        <div>
+            Built with <i class="far fa-heart"></i> at <a href="http://impactstory.org">Impactstory.</a>
+        </div>
+
+        <div>
+            Made possible by the support of <a href="https://www.arcadiafund.org.uk/">Arcadia</a> – a charitable fund of Lisbet Rausing and Peter Baldwin.
+
+        </div>
+
+
+    </v-footer>
 </template>
 
 <script>
@@ -65,54 +39,13 @@
 </script>
 
 <style scoped lang="scss">
-    .site-footer {
-        background: #333;
-        display: flex;
-        padding: 0 40px 100px 40px;
-        font-size: 14px;
-
-        .content {
-            width: 800px;
-            margin: 0 auto;
+    a {
+        color: #fff;
+        &.faq {
+            text-decoration: none;
+            font-weight: bold;
+            font-size: 75%;
         }
-
-        .col {
-            color: #fff;
-            margin-top: 50px;
-            a {
-                color: #fff;
-                &:hover {
-                    text-decoration: underline;
-                    color: #fff;
-                }
-            }
-            &.about {
-                div {
-                    margin: 5px 0;
-                    padding: 5px 0;
-                    /*border-bottom: 1px solid #666;*/
-                }
-            }
-        }
-
-        ul.contact-links {
-            margin: 0;
-            padding: 0;
-            li {
-                font-size: 22px;
-                display: inline-block;
-                list-style-type: none;
-                a {
-                    padding: 5px 10px;
-                }
-                span.text {
-                    display: none;
-                }
-            }
-        }
-
-
     }
-
 
 </style>
